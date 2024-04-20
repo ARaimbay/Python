6f3f1ed2e9041dbda1006ee672dd45fa40f4bef6
Aigerim Raimbay <raimbay89@gmail.com>
2024-04-19 21:37:21 -0500

    user input

---
+++ user_input/multiples_ten.py
@@ -0,0 +1,6 @@
+multiple_ten=input("Please write the number: ")
+multiple_ten=int(multiple_ten)
+if multiple_ten % 10 == 0:
+    print(f"Your number {multiple_ten} is a multiple of ten")
+else:
+    print(f"Your number {multiple_ten} is not a multiple of ten")
---
+++ user_input/rental_car.py
@@ -0,0 +1,2 @@
+preferred_car=input("What kind of rental car would you like? ")
+print(f"Let me see if I can find you a {preferred_car.title()}")

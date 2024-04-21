12277031a7a107a9c341d642a16781644f957dc3
Aigerim Raimbay <raimbay89@gmail.com>
2024-04-20 20:46:50 -0500

    first commit

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+message="Hello Python World!!!"
+print(message)
+
+message="Hello Python Crash Course world!!!"
+print(message)
+
+name="ada lovelace"
+print(name.title())
+
+name="ada lovelace"
+print(name.upper())
+
+first_name="ada"
+last_name="lovelace"
+full_name=f"{first_name} {last_name}"
+print(f"Hello,{full_name.title()}")
+
+
+message=f"Hello, {full_name.title()}!"
+print(message)
